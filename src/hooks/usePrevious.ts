a96014688981f7b91d74758777fd7897eedb44f6
Omar Jbara <omar.jbara2@gmail.com>
2019-03-10 21:48:12 +0100

    - Updated return type

--- src/hooks/usePrevious.ts
+++ src/hooks/usePrevious.ts
@@ -1,6 +1,6 @@
 import {useEffect, useRef} from "react"
 
-const usePrevious = <T>(value: T) => {
+const usePrevious = <T>(value: T): T |undefined => {
   const ref = useRef<T>()
 
   useEffect(() => {
